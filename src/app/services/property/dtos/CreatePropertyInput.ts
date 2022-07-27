52c18e17751ffa3935021d34639eb86122d0619b
Suat Oğulcan Özcan <ogulcan.ozcan@ithinka.com>
2022-07-27 17:59:42 +0300

    Added DisplayOnList and FilderOnList to Proprety

--- src/app/services/property/dtos/CreatePropertyInput.ts
+++ src/app/services/property/dtos/CreatePropertyInput.ts
@@ -20,4 +20,6 @@ export class CreatePropertyInput {
   isEnumProperty: boolean;
   enumerateId: number | undefined;
   enumerate: EnumerateFullOutput;
+  displayOnList: boolean;
+  filterOnList: boolean;
 }
